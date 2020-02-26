63676ea759e5b3fee43a5a398d4147843d8e4f1c
Maria <mariabrock414@gmail.com>
2020-02-25 18:34:52 -0600

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Howdy Y'all!!");
+
+            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+
+            Console.ReadKey(); // What happens if you run the app without this line?
+        }
+    }
+}
